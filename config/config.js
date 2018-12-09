5f88a2ea06b9922b0be8bd471be6c4603d52558c
Tim McGowan <30318479+timmcgowan@users.noreply.github.com>
2018-12-09 17:22:27 -0500

    use rds env vars

---
+++ config/config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    development: {
+      username: 'root',
+      password: 'root',
+      database: 'brocal_db',
+      host: '127.0.0.1',
+      dialect: 'mysql'
+    },
+    test: {
+        username: process.env.RDS_USERNAME,
+        password: process.env.RDS_PASSWORD,
+        database: 'brocal_db',
+        host: process.env.RDS_HOSTNAME,
+        port : process.env.RDS_PORT,
+      dialect: 'mysql'
+    },
+    production: {
+      username: process.env.RDS_USERNAME,
+      password: process.env.RDS_PASSWORD,
+      database: 'brocal_db',
+      host: process.env.RDS_HOSTNAME,
+      port : process.env.RDS_PORT,
+      dialect: 'mysql'
+    }
+}
