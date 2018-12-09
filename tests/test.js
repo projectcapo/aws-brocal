377dd443f58487f86c86cc49ce4cc2191d484ad5
Tim McGowan <30318479+timmcgowan@users.noreply.github.com>
2018-12-09 16:17:14 -0500

    ignore tests

--- tests/test.js
+++ tests/test.js
@@ -8,10 +8,10 @@ const request = supertest(app);
 
 describe('Tests app', function() {
   it('verifies get', function(done) {
-    //request.get('/').expect(200).end(function(err, result) {
+    request.get('/').expect(200).end(function(err, result) {
       //test.string(result.text).contains('Congratulations');
       //test.value(result).hasHeader('content-type', 'text/html');
-    //  done(err);
-    //});
+      done(err);
+    });
   });
 });
